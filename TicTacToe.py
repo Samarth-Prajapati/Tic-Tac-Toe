86bb2f2239c73ac9e963269ea8333557a6b1c9f1
Samarth Prajapati <samarthp475@gmail.com>
2024-08-26 15:46:34 +0530

    updated

--- TicTacToe.py
+++ TicTacToe.py
@@ -4,21 +4,38 @@ from tkinter import messagebox
 class TicTacToe:
     def __init__(self):
         self.root = tk.Tk()
-        self.root.title("Tic Tac Toe")
+        self.root.title("TIC TAC TOE")
         
         self.current_player = "X"
+
+        # _ - variable that will not be used
+
         self.board = [[' ' for _ in range(3)] for _ in range(3)]
+
+        # None - Buttons with None value
+        
         self.buttons = [[None for _ in range(3)] for _ in range(3)]
         
         for i in range(3):
             for j in range(3):
-                self.buttons[i][j] = tk.Button(self.root, text=' ', font=('Arial', 20), width=5, height=2,
-                                                command=lambda i=i, j=j: self.make_move(i, j))
+
+                # Create a Tkinter Button widget
+
+                # self.root - main window or frame
+                # command=lambda i=i, j=j: self.make_move(i, j) - This specifies the function to be called when the button is clicked. It uses a lambda function to capture the current values of i and j and then calls the self.make_move() method with these indices.
+
+                self.buttons[i][j] = tk.Button(self.root, text=' ', font=('Arial', 20), width=5, height=2,command=lambda i=i, j=j: self.make_move(i, j))
+
+                # .grid() method is used to place buttons within parent widgets
+
                 self.buttons[i][j].grid(row=i, column=j)
         
     def make_move(self, i, j):
         if self.board[i][j] == ' ':
             self.board[i][j] = self.current_player
+
+            # .config() method is used to modify the configuration options of a widget
+
             self.buttons[i][j].config(text=self.current_player)
             
             if self.check_winner():
@@ -58,6 +75,9 @@ class TicTacToe:
     
     def run(self):
         self.root.mainloop()
+
+# __name__ == "__main__" - Entry point of the script.
+# Creates an instance of the TicTacToe class and starts the game if the script is run directly.
 
 if __name__ == "__main__":
     game = TicTacToe()
